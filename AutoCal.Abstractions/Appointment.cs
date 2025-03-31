f0498a881405fca2e1a5571571cf6309aa011d1f
Thomas Hahn <thomas_d_hahn@t-online.de>
2025-03-31 18:30:46 +0000

    Change appointment time properties to use DateTimeOffset for better timezone handling

--- AutoCal.Abstractions/Appointment.cs
+++ AutoCal.Abstractions/Appointment.cs
@@ -5,8 +5,8 @@ public class Appointment
 {
     public string Id { get; set; }
     public string Title { get; set; }
-    public DateTime StartTime { get; set; }
-    public DateTime EndTime { get; set; }
+    public DateTimeOffset StartTime { get; set; }
+    public DateTimeOffset EndTime { get; set; }
     public string Location { get; set; }
     public string Description { get; set; }
     public BusyState BusyState { get; set; }
